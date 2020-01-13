4fbf50a577623ee3f65cc3c91d6472384aaa6764
Anton Sizikov <sizikov.anton@gmail.com>
2020-01-13 13:02:44 +0100

    probably shouln't have used thread.sleep here :)

--- src/GZipTest.Workflow/OrderedConcurrentDictionaryWrapper.cs
+++ src/GZipTest.Workflow/OrderedConcurrentDictionaryWrapper.cs
@@ -10,67 +10,62 @@ namespace GZipTest.Workflow
     {
         private readonly object syncObject = new object();
 
-        private readonly ConcurrentDictionary<long, ProcessedBatchItem> processedJobs =
-            new ConcurrentDictionary<long, ProcessedBatchItem>();
+        private readonly IDictionary<long, ProcessedBatchItem> processedJobs =
+            new Dictionary<long, ProcessedBatchItem>();
 
         private long lastTakenJobId = -1;
 
-        public IEnumerator<ProcessedBatchItem> GetEnumerator()
-            => processedJobs.Values.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public int Count => processedJobs.Count;
         public bool IsSynchronized { get; } = true;
         public object SyncRoot => syncObject;
 
+        public int Count
+        {
+            get
+            {
+                lock (syncObject)
+                {
+                    return processedJobs.Count;
+                }
+            }
+        }
+
+        public IEnumerator<ProcessedBatchItem> GetEnumerator() => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
         public void CopyTo(ProcessedBatchItem[] array, int index) => throw new NotImplementedException();
         public void CopyTo(Array array, int index) => throw new NotImplementedException();
         public ProcessedBatchItem[] ToArray() => throw new NotImplementedException();
 
-        public bool TryAdd(ProcessedBatchItem item) => processedJobs.TryAdd(item.JobBatchItemId, item);
+        public bool TryAdd(ProcessedBatchItem item)
+        {
+            lock (syncObject)
+            {
+                if (processedJobs.ContainsKey(item.JobBatchItemId))
+                {
+                    return false;
+                }
+
+                processedJobs.Add(item.JobBatchItemId, item);
+                Monitor.PulseAll(syncObject);
+                return true;
+            }
+        }
 
         public bool TryTake(out ProcessedBatchItem item)
         {
             lock (syncObject)
             {
                 var next = lastTakenJobId + 1;
-                if (processedJobs.TryRemove(next, out item))
+                while (!processedJobs.ContainsKey(next))
                 {
-                    lastTakenJobId++;
-                    return true;
+                    Monitor.Wait(syncObject);
                 }
 
-                var reset = new ManualResetEvent(false);
-                PollForNext(next, reset);
-                reset.WaitOne();
-
-                if (processedJobs.TryRemove(next, out item))
-                {
-                    lastTakenJobId++;
-                    return true;
-                }
-
-                return false;
+                item = processedJobs[next];
+                processedJobs.Remove(next);
+                lastTakenJobId++;
+                Monitor.PulseAll(syncObject);
+                return true;
             }
-        }
-
-        private void PollForNext(long next, EventWaitHandle reset)
-        {
-            var thread = new Thread(() =>
-            {
-                while (true)
-                {
-                    if (processedJobs.ContainsKey(next))
-                    {
-                        reset.Set();
-                        return;
-                    }
-
-                    Thread.Sleep(100);
-                }
-            });
-            thread.Start();
         }
     }
 }
